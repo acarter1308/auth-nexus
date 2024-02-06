1d2e88e4c18cbd7d2ecc5544687328a32d86ac23
Austin Carter <acarter@tdstickets.com>
2024-02-06 14:35:12 -0500

    small fix for toast component behavior

--- src/components/toast/PopIn.vue
+++ src/components/toast/PopIn.vue
@@ -27,20 +27,17 @@ defineExpose({ togglePopIn });
 .popin-container {
   position: absolute;
   text-align: center;
-  left: 101%;
+  left: 100%;
   top: 7%;
   font-weight: 600;
   border-radius: 10px;
   min-width: 225px;
-  max-width: 225px;
   opacity: 0%;
-  transition: all 1s ease-in-out;
+  transition: all 0.5s ease-in-out;
   background-color: var(--vue-green-dark);
   &.popped-in {
     opacity: 100%;
     left: 87%;
-    max-width: 225px;
-    min-width: 225px;
   }
   .popin-text {
     margin: 0.5rem;
